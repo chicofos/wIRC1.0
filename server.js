95becd307a3473b95ba1eb6f83a3cd14c68372c0
chico <fco.ochoas@gmail.com>
2014-11-25 17:36:22 -0700

    views, styles and passport login

--- server.js
+++ server.js
@@ -3,21 +3,57 @@ var app = express();
 var port = process.env.PORT || 8080;
 var expressLayouts = require('express-ejs-layouts');
 var morgan = require('morgan');
+var mongoose = require('mongoose');
+var passport = require('passport');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var session = require('express-session');
+var flash = require('connect-flash');
+var configDB = require('./config/database.js');
 
+
+//connect to our database
+mongoose.connect(configDB.url);
+
+// pass passport for configuration
+require('./config/passport')(passport); 
 
 //template
 app.set('view engine', 'ejs'); 
 app.set('layout', 'layout') 
-app.set('views', __dirname + '/views');
 
-app.use(express.static(__dirname + '/dist'));
+app.set('views', __dirname + '/public/views');
+
 
 //set our express application
 app.use(morgan('dev')); //log every request to the console
+app.use(cookieParser()); //read cookies (for auth)
+app.use(bodyParser()); //get information from html forms
 app.use(expressLayouts); //use multiple layouts
 
+//sessions
+app.use(session({
+    secret: 'a4f8071f-c873-4447-8ee2',
+    cookie: { maxAge: 2628000000 },
+    store: new (require('express-sessions'))({
+        storage: 'mongodb',
+        instance: mongoose, // optional
+        host: 'localhost', // optional
+        port: 27017, // optional
+        db: 'test', // optional
+        collection: 'sessions', // optional
+        expire: 86400 // optional
+    })
+}));
+
+app.use(passport.initialize());
+app.use(passport.session()); //persistent login sessions
+app.use(flash()); //for flash messages stored in session
+
+app.use(express.static(__dirname + '/public'));
+
 //routes module
-require('./src/js/routes')(app);
+require('./src/js/routes')(app,passport);
  
 //listen app
 app.listen(port);
--- src/js/routes.js
+++ src/js/routes.js
@@ -1,17 +1,74 @@
-module.exports = function(app){
+module.exports = function(app, passport){
 
 
-    //Home page
-    app.get('/', function(req, res){
-        res.render('home'); //load the home.ejs file
+   //Home page
+    app.get('/',isLoggedIn, function(req, res){
+        res.render('home', {user: req.user }); //load the home.ejs file
     });
 
     //Home page
-    app.get('/layout2', function(req, res){
-        res.render('home', { layout: 'layout2' }); //load the home.ejs file with layout 2
+    app.get('/login', function(req, res){
+        res.render('login', { layout : 'layout-login' }); //load the home.ejs file
+    });
+
+    //Profile
+    app.get('/profile', isLoggedIn, function(req, res){
+        //get the user out of session and pass to template
+        res.render('profile', { user : req.user });
+    });
+
+     // FACEBOOK ROUTES ======================
+
+    // route for facebook authentication and login
+    app.get('/auth/facebook', passport.authenticate('facebook', { scope : 'email' }));
+
+    // handle the callback after facebook has authenticated the user
+    app.get('/auth/facebook/callback',
+        passport.authenticate('facebook', {
+            successRedirect : '/',
+            failureRedirect : '/'
+        }));
+
+    // TWITTER ROUTES ======================
+        //TODO
+
+
+    // GOOGLE ROUTES ======================
+        //TODO
+
+    // route for logging out
+    app.get('/logout', function(req, res) {
+        req.logout();
+        res.redirect('/');
+    });
+
+    //Handle Error 404
+    app.get('*', function(req, res, next) {
+      var err = new Error();
+      err.status = 404;
+      next(err);
+    });
+
+    // handling 404 errors
+    app.use(function(err, req, res, next) {
+      if(err.status !== 404) {
+        return next();
+      }
+      res.render('404' , {layout : 'layout-login'});
     });
 
 };
 
+
+//route middleware to make sure a user is logged index
+function isLoggedIn(req, res, next){
+
+    //if the user is authenticated in the session, carry on
+    if(req.isAuthenticated())
+        return next();
+
+    //if they aren't redirect them to the login page
+    res.redirect('/login');
+}
 
 
